48d2d3d692143405183d2d20d902d3b91a6e2ca0
Enes Baysal <76823958+n3sB@users.noreply.github.com>
2024-12-03 10:48:37 +0300

    Update order_executor.py

--- order_executor.py
+++ order_executor.py
@@ -1,7 +1,6 @@
 from binance.enums import *
 
 def execute_order(client, symbol, side, quantity):
-    """Place an order."""
     try:
         print(f"Placing {side} order for {symbol}")
         order = client.create_order(symbol=symbol, side=side, type=ORDER_TYPE_MARKET, quantity=quantity)
