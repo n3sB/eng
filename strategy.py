c6c4a4145504e429135ccdf5150062ab4d5fdb2b
Enes Baysal <76823958+n3sB@users.noreply.github.com>
2024-12-03 10:46:37 +0300

    first commit

---
+++ strategy.py
@@ -0,0 +1,16 @@
+from indicators import calculate_rsi, calculate_macd
+
+def evaluate_strategy(symbol, closes, config, position):
+    """Evaluate trading signals."""
+    rsi = calculate_rsi(closes, config.RSI_PERIOD)
+    macd_hist = calculate_macd(closes)[2]
+
+    if len(closes) > config.RSI_PERIOD:
+        last_rsi = rsi[-1]
+        last_macd_hist = macd_hist[-1]
+
+        if last_rsi < config.RSI_OVERSOLD and last_macd_hist > 0 and not position['in_position']:
+            return "BUY"
+        elif last_rsi > config.RSI_OVERBOUGHT and last_macd_hist < 0 and position['in_position']:
+            return "SELL"
+    return "HOLD"
